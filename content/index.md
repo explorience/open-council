a80e76e3b753568e2129afe6685ffeb25e72e5d9
Garklein <garklein97@gmail.com>
2025-08-06 10:45:29 -0400

    put explorer on right

--- content/index.md
+++ content/index.md
@@ -1,3 +1,5 @@
 ---
 title: Open Council
 ---
+
+London's meeting minutes should be easy to read.
--- quartz.layout.ts
+++ quartz.layout.ts
@@ -6,12 +6,39 @@ export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
   afterBody: [],
-  footer: Component.Footer({
-    links: {
-      GitHub: "https://github.com/explorience/open-council",
-    },
-  }),
+  footer: Component.Footer()
 }
+
+const explorer = Component.Explorer({
+  folderDefaultState: "collapsed",
+
+  // use default sorting, but compare with slugs instead of displayNames and reverse (newest first)
+  sortFn: (a, b) => {
+    if ((!a.isFolder && !b.isFolder) || (a.isFolder && b.isFolder)) {
+      // numeric: true: Whether numeric collation should be used, such that "1" < "2" < "10"
+      // sensitivity: "base": Only strings that differ in base letters compare as unequal. Examples: a ≠ b, a = á, a = A
+      return -a.slug.localeCompare(b.slug, undefined, {
+        numeric: true,
+        sensitivity: "base",
+      })
+    }
+
+    return (!a.isFolder && b.isFolder) ? 1 : -1
+  },
+  mapFn: (node) => {
+    if (node.isFolder) return
+
+    const dateStr = node.slugSegments[1].slice(0, "YYYY-MM-DD".length)
+    const fmtOptions = {
+      year: "numeric",
+      month: "short",
+      day: "numeric"
+    }
+    // switch `-` to `/` to force it to parse in the current time zone
+    const date = new Date(dateStr.replace("-", "/")).toLocaleDateString("en-CA", fmtOptions)
+    node.displayName = `${node.displayName} (${date})`
+  }
+})
 
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
@@ -27,38 +54,15 @@ export const defaultContentPageLayout: PageLayout = {
   ],
   afterBody: [
     Component.ConditionalRender({
-      component: Component.Explorer({
-        folderDefaultState: "collapsed",
-
-        // use default sorting, but compare with slugs instead of displayNames and reverse (newest first)
-        sortFn: (a, b) => {
-          if ((!a.isFolder && !b.isFolder) || (a.isFolder && b.isFolder)) {
-            // numeric: true: Whether numeric collation should be used, such that "1" < "2" < "10"
-            // sensitivity: "base": Only strings that differ in base letters compare as unequal. Examples: a ≠ b, a = á, a = A
-            return -a.slug.localeCompare(b.slug, undefined, {
-              numeric: true,
-              sensitivity: "base",
-            })
-          }
-
-          return (!a.isFolder && b.isFolder) ? 1 : -1
-        },
-        mapFn: (node) => {
-          if (node.isFolder) return
-
-          const dateStr = node.slugSegments[1].slice(0, "YYYY-MM-DD".length)
-          const fmtOptions = {
-            year: "numeric",
-            month: "short",
-            day: "numeric"
-          }
-          // switch `-` to `/` to force it to parse in the current time zone
-          const date = new Date(dateStr.replace("-", "/")).toLocaleDateString("en-CA", fmtOptions)
-          node.displayName = `${node.displayName} (${date})`
-        }
+      component: Component.Flex({
+        direction: "column",
+        components: [
+          { Component: Component.Search() },
+          { Component: explorer }
+        ]
       }),
       condition: (page) => page.fileData.slug === "index",
-    }),
+    })
   ],
   left: [
     Component.ConditionalRender({
@@ -71,7 +75,11 @@ export const defaultContentPageLayout: PageLayout = {
     }),
     Component.TableOfContents()
   ],
-  right: [],
+  right: [
+    Component.ConditionalRender({
+      component: explorer,
+      condition: (page) => page.fileData.slug !== "index"
+    })],
 }
 
 // components for pages that display lists of pages  (e.g. tags or folders)
@@ -89,7 +97,6 @@ export const defaultListPageLayout: PageLayout = {
         { Component: Component.Darkmode() },
       ],
     }),
-    Component.Explorer()
   ],
-  right: [],
+  right: [explorer],
 }
